a90171b5691d47c7a02ec4ad8f8810db7b2fb8bc
Elisa <elisa-devops@users.noreply.github.com>
2024-10-01 10:16:59 -0500

    added pumpkin ascii art and if statements to check user input

--- FunFunctions.kt
+++ FunFunctions.kt
@@ -1,3 +1,6 @@
+//Project: Fun Functions Kotlin Project
+//Purpose: Create functions that are fun to call/ invoke
+//Author: Elisa Aldridge
 
 fun smores() {
     println("Roast a marshmallow.")
@@ -6,10 +9,53 @@ fun smores() {
     println("Put a new graham cracker on chocolate.")
     println("Enjoy!")
   }
+
+fun noSmores() {
+    println("I'm sorry to hear you don't want to learn to make a smore.")
+}
+
+fun greeting() {
+    println("Hello Friend, Happy October 1st")
+    println("Would you like to learn to make a Smore? y/n")
+}
+
+fun pumpkin() {
+    println("                  @@@")
+    println("                   @@@")
+    println("                    @@@                       H A P P Y")
+    println("                    @@@")
+    println("           @@@@@@@@@@@@@@@@@@@@@@         H A L L O W E E N")
+    println("         @@@@@@@@@@@@@@@@@@@@@@@@@@")
+    println("       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    println("     @@@@@@@@ @@@@@@@@@@@@@@@@ @@@@@@@@")
+    println("   @@@@@@@@@   @@@@@@@@@@@@@@   @@@@@@@@@")
+    println(" @@@@@@@@@@     @@@@@@@@@@@@     @@@@@@@@@@")
+    println("@@@@@@@@@@       @@@@  @@@@       @@@@@@@@@@")
+    println("@@@@@@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@@@@@")
+    println("@@@@@@@@@@@@@@@@@@        @@@@@@@@@@@@@@@@@@")
+    println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    println("@@@@@@@@@ @@@@@@@@@@@@@@@@@@@@@@@@ @@@@@@@@@")
+    println(" @@@@@@@@  @@ @@ @@ @@ @@ @@ @@ @  @@@@@@@@")
+    println("   @@@@@@@                        @@@@@@@")
+    println("     @@@@@@  @@ @@ @@ @@ @@ @@ @ @@@@@@")
+    println("      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    println("        @@@@@@@@@@@@@@@@@@@@@@@@@@")
+    println("          @@@@@@@@@@@@@@@@@@@@@@")
+}
   
   
   fun main() {
-    smores()
-    smores()
+    greeting()
+
+    //user asked if wants recipe for smore
+    var x = readLine()
+    println(x)
+    if (x == "y" || x == "yes" || x == "ye") {
+        smores()
+    } else if (x == "n" || x == "no") {
+        noSmores()
+    }
+    pumpkin()
   }
   
